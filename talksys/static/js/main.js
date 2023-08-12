ab68502ad46a5172f78281b5a43bc9b60986b10c
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-12 18:56:49 +0300

    Set Up Javascript to Navigate in Bubble

---
+++ talksys/static/js/main.js
@@ -0,0 +1,36 @@
+
+// Variables
+let chatName = ''
+let chatSocket = null
+let chatWindowUrl = window.location.href
+let chatRoomUuid = Math.random().toString(36).slice(2,12)
+
+// Elements to manipulate
+const chatElement = document.querySelector('#chat')
+const chatOpenElement = document.querySelector('#chat_open')
+const chatJoinElement = document.querySelector('#chat_join')
+const chatIconElement = document.querySelector('#chat_icon')
+const chatWelcomeElement = document.querySelector('#chat_welcome')
+const chatRoomElement = document.querySelector('#chat_room')
+
+
+
+// Event Listeners 
+
+chatOpenElement.onclick = function(e) {
+    e.preventDefault()
+
+    chatIconElement.classList.add('hidden')
+    chatWelcomeElement.classList.remove('hidden')
+
+    return false
+} 
+
+chatJoinElement.onclick = function(e) {
+    e.preventDefault()
+
+    chatWelcomeElement.classList.add('hidden')
+    chatRoomElement.classList.remove('hidden')
+
+    return false
+}
