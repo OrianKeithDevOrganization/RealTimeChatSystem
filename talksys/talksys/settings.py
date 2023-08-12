2400c4d4616cc688c97e7001206f7cea1f3edcb0
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-12 14:03:58 +0300

    Tested and working files

--- talksys/talksys/settings.py
+++ talksys/talksys/settings.py
@@ -120,9 +120,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIR = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+###################### My SETTINGS  #####################################
+
+AUTH_USER_MODEL = 'account.User'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL  = '/chat-admin/'
+LOGOUT_REDIRECT_URL = '/' 
+ASGI_APPLICATION = 'talksys.asgi.application' # deals with asynchronous connections. 
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND':'channels.layers.InMemoryChannelLayer',
+    }
+}
