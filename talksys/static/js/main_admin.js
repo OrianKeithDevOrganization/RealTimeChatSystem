570c9fb598d4a5c507a7e0ad1a6f1b980193c1bf
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-13 20:06:05 +0300

    Chat Administration Part 2 - Connect to web socket in chat admin

--- talksys/static/js/main_admin.js
+++ talksys/static/js/main_admin.js
@@ -5,3 +5,26 @@ const chatRoom = document.querySelector('#room_uuid').textContent.replace('"',''
 
 let chatSocket = null 
 
+// Elements 
+
+const chatLogElement = document.querySelector('#chat_log')
+const chatInputElement = document.querySelector('#chat_message_input')
+const chatSubmitlement = document.querySelector('#chat_message_submit')
+ 
+// Setup the web socket 
+
+chatSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoom}/`) 
+
+// Adding an event listener
+
+chatSocket.onmessage = function(e) {
+    console.log('on message')
+} 
+
+chatSocket.onopen = function(e) {
+    console.log('on open')
+} 
+
+chatSocket.onclose = function(e) {
+    console.log('Sorry the chat socket closed unexpectedly ')
+}
