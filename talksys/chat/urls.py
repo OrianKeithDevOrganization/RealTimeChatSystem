8d6966352ab80265c8a338c0d15ff63403292299
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-14 00:27:54 +0300

    User Management Complete CRUD

--- talksys/chat/urls.py
+++ talksys/chat/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path 
-from .views import create_room, admin, room
+from .views import create_room, admin, room, add_user,user_detail,edit_user
 
 app_name = 'chat'
 
 urlpatterns = [
     path('api/create-room/<str:uuid>/',create_room,name='create-room'),
     path('chat-admin/', admin, name='admin'),
+    path('chat-admin/add-user/',add_user, name='add_user'),
+    path('chat-admin/users/<uuid:uuid>/', user_detail, name='user_detail'), 
+    path('chat-admin/users/<uuid:uuid>/edit/', edit_user, name='edit_user'), 
     path('chat-admin/<str:uuid>/',room, name='room'),
 ]
--- talksys/chat/views.py
+++ talksys/chat/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import json
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST 
 from .models import Room
 from django.contrib.auth.decorators import login_required
-from account.models import User
+from account.models import User 
+from account.forms import AddUserForm, EditUserForm 
+from django.contrib import messages
+from django.contrib.auth.models import Group
 
 # Create your views here.
 
@@ -41,3 +44,66 @@ def room(request, uuid):
     }
     return render(request,'chat/room.html',context=context) 
 
+
+@login_required
+def add_user(request):
+    if request.user.has_perm('user.add_user'):
+        if request.method == 'POST':
+            form = AddUserForm(request.POST)
+
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_staff = True
+                user.set_password(request.POST.get('password'))
+                user.save() #saving user to database 
+
+                if user.role == User.MANAGER:
+                    group = Group.objects.get(name='Managers')
+                    group.user_set.add(User) 
+                messages.success(request,'New user added Successfully!')
+                return redirect('/chat-admin/')
+        else: 
+            form = AddUserForm()
+            context = {
+                'form':form
+            }
+        return render(request, 'chat/add_user.html', context=context)
+    else: 
+        messages.error(request, "You don't have access to add user !")
+        return redirect('/chat-admin/') 
+    
+
+@login_required
+def user_detail(request,uuid):
+    user = User.objects.get(pk=uuid)
+    rooms = user.rooms.all() # getting rooms (with related name) associated to user
+    context = {
+        'user':user,
+        'rooms':rooms
+    }
+    return render(request,'chat/user_detail.html',context=context)
+
+@login_required
+def edit_user(request,uuid):
+    if request.user.has_perm('user.edit_user'):
+        user = User.objects.get(pk=uuid)
+
+        if request.method == 'POST':
+            form = EditUserForm(request.POST,instance=user) #prefilled data form
+            
+            if form.is_valid():
+                form.save() 
+                messages.success(request,'Changes were saved Successfully !')
+                return redirect('/chat-admin/')
+            
+        else: 
+            form = EditUserForm(instance=user)
+        context = {
+            'user':user,
+            'form':form
+        }
+        return render(request,'chat/edit_user.html',context=context)
+    else:
+        messages.error(request, "You don't have access to edit users !")
+        return redirect('/chat-admin/') 
+            
